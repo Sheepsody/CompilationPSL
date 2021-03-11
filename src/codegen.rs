768f2b6c93501e36fc31b7bc308e2bd686976f9a
Victor Vialard <victor.vialard@mines-paristech.fr>
2021-03-11 12:24:10 +0100

    Can use variables

--- src/codegen.rs
+++ src/codegen.rs
@@ -16,21 +16,36 @@ pub struct CodeGen<'ctx> {
     pub builder: Builder<'ctx>,
     pub module: Module<'ctx>,
     pub execution_engine: ExecutionEngine<'ctx>,
+    variables: HashMap<String, FloatValue<'ctx>>,
     // variables: HashMap<String, PointerValue<'ctx>>,
     // fn_value_opt: Option<FunctionValue<'ctx>>,
 }
 
 impl<'ctx> CodeGen<'ctx> {
-    pub fn jit_compile_sum(&self, node: &Node) -> Result<FloatValue<'ctx>, &'static str> {
+    pub fn jit_compile(&mut self, node: &Node) -> Result<FloatValue<'ctx>, &'static str> {
         match node {
             Node::NumberExpr(nb) => Ok(self.context.f64_type().const_float(*nb)),
+            Node::IdentExpr(name) => match self.variables.get(name.as_str()) {
+                Some(value) => Ok(*value),
+                None => Err("Could not find a matching variable."),
+            },
             Node::BinaryExpr { op, lhs, rhs } => {
-                let lhs = self.jit_compile_sum(lhs).unwrap();
-                let rhs = self.jit_compile_sum(rhs).unwrap();
+                let lhs = self.jit_compile(lhs).unwrap();
+                let rhs = self.jit_compile(rhs).unwrap();
                 match op {
                     Op::Add => Ok(self.builder.build_float_add(lhs, rhs, "tmpadd")),
                     // TODO: Add other ops
                     _ => unimplemented!(),
+                }
+            }
+            Node::InitExpr { ident, expr } => {
+                if let Node::IdentExpr(name) = ident.as_ref() {
+                    let expr = self.jit_compile(expr);
+                    self.variables.insert(name.clone(), expr.unwrap());
+                    //                    self.builder.build_store(alloca, expr.unwrap());
+                    return expr;
+                } else {
+                    unimplemented!()
                 }
             }
             _ => unimplemented!(),
@@ -39,26 +54,33 @@ impl<'ctx> CodeGen<'ctx> {
 }
 
 pub fn execute(string: &str) -> f64 {
-    use super::parse;
+    use super::parser::parse;
+
     let context = Context::create();
     let module = context.create_module("sum");
     let execution_engine = module
         .create_jit_execution_engine(OptimizationLevel::None)
         .unwrap();
-    let codegen = CodeGen {
+    let mut codegen = CodeGen {
         context: &context,
         module,
         builder: context.create_builder(),
         execution_engine,
+        variables: HashMap::new(),
     };
 
-    codegen
-        .jit_compile_sum(parse(string).get(0).unwrap())
-        .ok()
-        .unwrap()
-        .get_constant()
-        .unwrap()
-        .0
+    let mut result = 0.0;
+    for line in parse(string) {
+        result = codegen
+            .jit_compile(&line)
+            .ok()
+            .unwrap()
+            .get_constant()
+            .unwrap()
+            .0
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -73,5 +95,15 @@ mod codegen {
     #[test]
     fn add() {
         assert_eq!(execute("1+2;"), 3.0)
+    }
+
+    #[test]
+    fn var_init() {
+        assert_eq!(execute("let a = 2+2;"), 4.0)
+    }
+
+    #[test]
+    fn var_use() {
+        assert_eq!(execute("let a = 10; a+3;"), 13.0)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,6 @@ extern crate pest_derive;
 #[macro_use]
 extern crate lazy_static;
 
-use codegen::CodeGen;
-use parser::parse;
-
-use inkwell::context::Context;
-use inkwell::OptimizationLevel;
-
 fn main() -> Result<(), ()> {
-    let context = Context::create();
-    let module = context.create_module("sum");
-    let execution_engine = module
-        .create_jit_execution_engine(OptimizationLevel::None)
-        .unwrap();
-    let codegen = CodeGen {
-        context: &context,
-        module,
-        builder: context.create_builder(),
-        execution_engine,
-    };
-
-    let sum = codegen.jit_compile_sum(parse("1;").get(0).unwrap()).ok();
-
-    println!("{}", sum.unwrap().get_constant().unwrap().0);
-
     Ok(())
 }
