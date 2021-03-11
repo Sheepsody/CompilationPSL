1a26d23e211640e5c1fff36fa6cc3d105fae8362
Victor Vialard <victor.vialard@mines-paristech.fr>
2021-03-11 19:50:04 +0100

    Added conditionnal expressions

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,3 +1,7 @@
+// TODO
+// Declare new functions (no access to global variables)
+// Call functions
+
 use super::ast::{Node, Op};
 use std::collections::HashMap;
 
@@ -12,24 +16,27 @@ use inkwell::{FloatPredicate, OptimizationLevel};
 
 type JitFunc = unsafe extern "C" fn() -> f64;
 
-struct RecursiveBuilder<'ctx> {
+struct RecursiveBuilder<'a, 'ctx> {
     f64_type: FloatType<'ctx>,
-    builder: &'ctx Builder<'ctx>,
+    builder: &'a Builder<'ctx>,
+    module: &'a Module<'ctx>,
     context: &'ctx Context,
-    variables: HashMap<String, PointerValue<'ctx>>,
-    fn_value: &'ctx FunctionValue<'ctx>,
+    pub variables: HashMap<String, PointerValue<'ctx>>,
+    fn_value: &'a FunctionValue<'ctx>,
 }
 
-impl<'ctx> RecursiveBuilder<'ctx> {
+impl<'a, 'ctx> RecursiveBuilder<'a, 'ctx> {
     pub fn new(
         f64_type: FloatType<'ctx>,
         context: &'ctx Context,
-        builder: &'ctx Builder,
-        fn_value: &'ctx FunctionValue,
+        module: &'a Module<'ctx>,
+        builder: &'a Builder<'ctx>,
+        fn_value: &'a FunctionValue<'ctx>,
     ) -> Self {
         Self {
             f64_type,
             builder,
+            module,
             context,
             variables: HashMap::new(),
             fn_value,
@@ -95,7 +102,65 @@ impl<'ctx> RecursiveBuilder<'ctx> {
                     unimplemented!()
                 }
             }
-            _ => unimplemented!(),
+
+            Node::CondExpr { cond, cons, alter } => {
+                let parent = *self.fn_value;
+                let zero_const = self.context.f64_type().const_float(0.0);
+
+                // create condition by comparing without 0.0 and returning an int
+                let cond = self.build(cond)?;
+                let cond = self.builder.build_float_compare(
+                    FloatPredicate::ONE,
+                    cond,
+                    zero_const,
+                    "ifcond",
+                );
+
+                // build branch
+                let then_bb = self.context.append_basic_block(parent, "then");
+                let else_bb = self.context.append_basic_block(parent, "else");
+                let cont_bb = self.context.append_basic_block(parent, "ifcont");
+
+                self.builder
+                    .build_conditional_branch(cond, then_bb, else_bb);
+
+                // build then block
+                self.builder.position_at_end(then_bb);
+                let then_val = self.build(cons)?;
+                self.builder.build_unconditional_branch(cont_bb);
+
+                let then_bb = self.builder.get_insert_block().unwrap();
+
+                // build else block
+                self.builder.position_at_end(else_bb);
+                // FIXME
+                let mut else_val = self.f64_type.const_float(0.0);
+                if let Some(node) = alter {
+                    else_val = self.build(node).unwrap();
+                }
+                self.builder.build_unconditional_branch(cont_bb);
+
+                let else_bb = self.builder.get_insert_block().unwrap();
+
+                // emit merge block
+                self.builder.position_at_end(cont_bb);
+
+                let phi = self.builder.build_phi(self.context.f64_type(), "iftmp");
+
+                phi.add_incoming(&[(&then_val, then_bb), (&else_val, else_bb)]);
+
+                Ok(phi.as_basic_value().into_float_value())
+            }
+
+            Node::BlockExpr(nodes) => {
+                let mut result: Result<FloatValue, &str> = Result::Err("No return specified");
+                for node in nodes {
+                    result = self.build(node);
+                }
+                result
+            }
+
+            _ => unimplemented!("{:?}", node),
         }
     }
 }
@@ -111,8 +176,8 @@ pub fn execute(string: &str) -> f64 {
         .create_jit_execution_engine(OptimizationLevel::None)
         .unwrap();
 
-    let i64_type = context.f64_type();
-    let fn_type = i64_type.fn_type(&[], false);
+    let f64_type = context.f64_type();
+    let fn_type = f64_type.fn_type(&[], false);
     let function = module.add_function("jit", fn_type, None);
 
     let basic_block = context.append_basic_block(function, "entry");
@@ -120,17 +185,22 @@ pub fn execute(string: &str) -> f64 {
 
     let mut result: Result<FloatValue, &str> = Result::Err("No return specified");
 
-    let mut recursive_builder = RecursiveBuilder::new(i64_type, &context, &builder, &function);
+    let mut recursive_builder =
+        RecursiveBuilder::new(f64_type, &context, &module, &builder, &function);
     for node in parse(string) {
         result = recursive_builder.build(&node);
     }
     builder.build_return(Some(&result.ok().unwrap()));
 
+    module.print_to_stderr();
+
+    // println!("{:?}", function.print_to_stderr());
     unsafe {
         let jit_function: JitFunction<JitFunc> = execution_engine.get_function("jit").unwrap();
         jit_function.call()
     }
 }
+
 #[cfg(test)]
 mod codegen {
     use super::execute;
@@ -159,4 +229,19 @@ mod codegen {
     fn var_assign() {
         assert_eq!(execute("let a = 10; a = 8-3; a+3;"), 8.0)
     }
+
+    #[test]
+    fn cond_true() {
+        assert_eq!(execute("if (1) then {1+3;} else {1;};"), 4.0)
+    }
+
+    #[test]
+    fn cond_false() {
+        assert_eq!(execute("if (0) then {1+3;} else {1;};"), 1.0)
+    }
+
+    //    #[test]
+    //    fn fn_decl() {
+    //        assert_eq!(execute("fn test() {1;};"), 0.0)
+    //    }
 }
